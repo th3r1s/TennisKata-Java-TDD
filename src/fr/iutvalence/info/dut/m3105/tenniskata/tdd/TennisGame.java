72ccdc82690d1c339d8f368cd1c98e9b9657328c
silvesta <silvesta@PC-B103-01.iut-valence.fr>
2015-10-23 08:50:35 +0200

    Green (love-all)

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -3,6 +3,20 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
 	
+	private int score1;
+	private int score2;
+	
+	public TennisGame() {
+		score1 = 0;
+		score2 = 0;
+	}
+	
+	public String getScore(){
+		if(score1 == 0 && score2 == 0)
+			return "love-all";
+		return null;
+	}
+	
 	
 	
 }
