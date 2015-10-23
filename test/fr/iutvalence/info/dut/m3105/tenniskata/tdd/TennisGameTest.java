af6c440f1579fe833fd7e71d842f6ce622ff915a
silvesta <silvesta@PC-B103-01.iut-valence.fr>
2015-10-23 09:34:16 +0200

    Last commit, not finished

--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -8,32 +8,74 @@ public class TennisGameTest
 	@Test
 	public void loveAllTest()
 	{
-		TennisGame tg = new TennisGame();
-		Assert.assertEquals(tg.getScore(), Scores.LOVE_ALL);		
-	}
-	
-	@Test
-	public void fifteenLoveTest(){
-		TennisGame tg = new TennisGame();
-		tg.serverScore();
-		Assert.assertEquals(tg.getScore(), Scores.FIFTEEN_LOVE);
-	}
-	
-	@Test
-	public void loveFifteenTest(){
-		TennisGame tg = new TennisGame();
+		TennisGame tg = new TennisGame();	
+		
+		Assert.assertEquals(tg.getScore(), Scores.LOVE_ALL);
+				
 		tg.receiverScore();
 		Assert.assertEquals(tg.getScore(), Scores.LOVE_FIFTEEN);
-	}
-	
-	@Test
-	public void fifteenAllTest(){
-		TennisGame tg = new TennisGame();
+		
+		tg.receiverScore();
+		Assert.assertEquals(tg.getScore(), Scores.LOVE_THIRTY);
+		
+		tg.receiverScore();
+		Assert.assertEquals(tg.getScore(), Scores.LOVE_FORTY);
+		
+		
+		tg.resetScores();
+		
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.FIFTEEN_LOVE);
+		
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.THIRTY_LOVE);
+		
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.FORTY_LOVE);
+		
+		
+		
+		tg.resetScores();
+				
 		tg.receiverScore();
 		tg.serverScore();
 		Assert.assertEquals(tg.getScore(), Scores.FIFTEEN_ALL);
-	}
+		
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.THIRTY_FIFTEEN);
+		
+		tg.receiverScore();
+		Assert.assertEquals(tg.getScore(), Scores.THIRTY_ALL);
+		
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.FORTY_THIRTY);
+		
+		
+		
+		tg.resetScores();
+		
+		tg.receiverScore();
+		tg.receiverScore();
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.FIFTEEN_THIRTY);
+		
+		tg.receiverScore();
+		tg.serverScore();
+		Assert.assertEquals(tg.getScore(), Scores.THIRTY_FORTY);
+		
+		
+		
+		
+		
+//		for(int server=0; server<4; server++){
+//			for(int receiver=0; receiver<4; receiver++){
+//				
+//			}
+//		}
+		
+		
 
-	
-	
+		
+	}
+		
 }
